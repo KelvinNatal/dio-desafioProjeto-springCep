ac61393da4bed2f7a12e53fbe05b13fe852d4642
KelvinNatal <kelvinnatal@hotmail.com>
2023-04-15 17:20:15 -0300

    Final Project

---
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+
+/*
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente
+ */
+
+public interface ClienteService {
+    
+    Iterable<Cliente> burcarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
